bce640db1c6ea8dfe09f08c4966488bc29520b3c
G Sh <conan@yandex.ru>
2025-04-19 17:00:40 +0300

    рефакторинг

--- methods/login_method.py
+++ methods/login_method.py
@@ -1,7 +1,6 @@
 import requests
 import data
-import random
-import string
+
 
 class LoginMethods:
 
@@ -29,8 +28,8 @@ class LoginMethods:
     @staticmethod
     def login_in_system(login, password):
         params = {'login': login, 'password': password}
-        responce = requests.post(f'{data.Url.MAIN_URL}{data.Url.LOGIN_COURIER_URL}/', data = params )
-        courier_id = responce.json()
+        response = requests.post(f'{data.Url.MAIN_URL}{data.Url.LOGIN_COURIER_URL}/', data = params )
+        courier_id = response.json()
         return courier_id["id"]
 
 
--- tests/test_create_login.py
+++ tests/test_create_login.py
@@ -1,55 +1,54 @@
 import allure
-from methods.login_method import LoginMethods
 
 
 class TestCreateLogin:
     @allure.title('Test Successful login creation')
     @allure.description('Тут создаем логин и проверяем ответ API')
     def test_create_login(self,generate_login_data):
-        responce = generate_login_data
-        assert responce.status_code == 201 and responce.json()['ok'] == True
+        response = generate_login_data
+        assert response.status_code == 201 and response.json()['ok'] == True
 
     @allure.title('Test UnSuccessful two login creation with the same data')
     @allure.description('Тут пытаемся создать два курьера с одинаковыми логинами и проверяем ответ API')
     def test_create_two_courier_with_same_data(self, generate_two_courier_with_same_login_data):
-        responce = generate_two_courier_with_same_login_data
-        assert responce.status_code == 409 and responce.json()['message'] == 'Этот логин уже используется. Попробуйте другой.'
+        response = generate_two_courier_with_same_login_data
+        assert response.status_code == 409 and response.json()['message'] == 'Этот логин уже используется. Попробуйте другой.'
 
     @allure.title('Test UnSuccessful create courier without login data')
     @allure.description('Пытаемся создать курьера без логина и проверяем ответ API')
     def test_create_courier_without_login(self,generate_without_login_data):
-        responce = generate_without_login_data
-        assert responce.status_code == 400 and responce.json()['message'] == 'Недостаточно данных для создания учетной записи'
+        response = generate_without_login_data
+        assert response.status_code == 400 and response.json()['message'] == 'Недостаточно данных для создания учетной записи'
 
     @allure.title('Test UnSuccessful create courier without password data')
     @allure.description('Пытаемся создать курьера без пароля и проверяем ответ API')
     def test_create_courier_without_password(self, generate_without_password_data):
-        responce = generate_without_password_data
-        assert responce.status_code == 400 and responce.json()[
+        response = generate_without_password_data
+        assert response.status_code == 400 and response.json()[
             'message'] == 'Недостаточно данных для создания учетной записи'
 
     @allure.title('Courier creation returning code 201')
     @allure.description('Проверка, что запрос возвращает правильный код ответа;')
     def test_create_login_ok(self,generate_login_data):
-        responce = generate_login_data
-        assert responce.status_code == 201
+        response = generate_login_data
+        assert response.status_code == 201
 
     @allure.title('Test returning error without some data in body ')
     @allure.description('Если нет поля "логин", запрос возвращает ошибку;')
     def test_error_create_courier_without_login(self, generate_without_login_data):
-        responce = generate_without_login_data
-        assert responce.status_code == 400 and responce.json()['message'] == 'Недостаточно данных для создания учетной записи'
+        response = generate_without_login_data
+        assert response.status_code == 400 and response.json()['message'] == 'Недостаточно данных для создания учетной записи'
 
 
     @allure.title('Test returning error without some data in body ')
     @allure.description('Если нет поля "пароль", запрос возвращает ошибку;')
     def test_error_create_courier_without_password(self, generate_without_password_data):
-        responce = generate_without_password_data
-        assert responce.status_code == 400 and responce.json()['message'] == 'Недостаточно данных для создания учетной записи'
+        response = generate_without_password_data
+        assert response.status_code == 400 and response.json()['message'] == 'Недостаточно данных для создания учетной записи'
 
     @allure.title('Test UnSuccessful two login creation with the same data')
     @allure.description('Если создать пользователя с логином, который уже есть, возвращается ошибка.')
     def test_create_two_courier_with_same_login_data(self, generate_two_courier_with_same_login_data):
-        responce = generate_two_courier_with_same_login_data
-        assert responce.status_code == 409 and responce.json()['message'] == 'Этот логин уже используется. Попробуйте другой.'
+        response = generate_two_courier_with_same_login_data
+        assert response.status_code == 409 and response.json()['message'] == 'Этот логин уже используется. Попробуйте другой.'
 
