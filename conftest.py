e633de6ac640255dfb0d22fb9b5a54d321111699
G Sh <conan@yandex.ru>
2025-04-20 12:33:47 +0300

    Модернизировал метод залогинивания.

--- conftest.py
+++ conftest.py
@@ -8,14 +8,14 @@ def generate_login_data():
     login_body = LoginMethods.register_new_courier_and_return_login_password(test_body)
     body = login_body[3]
     yield body
-    LoginMethods.delete_login(LoginMethods.login_in_system(login_body[0], login_body[1])[0])
+    LoginMethods.delete_login(LoginMethods.login_in_system(login_body[0], login_body[1]).json()["id"])
 
 @pytest.fixture
 def generate_two_courier_with_same_login():
     login_body = LoginMethods.register_two_courier_with_same_login()
     body = login_body[4]
     yield body
-    LoginMethods.delete_login(LoginMethods.login_in_system(login_body[0], login_body[1])[0])
+    LoginMethods.delete_login(LoginMethods.login_in_system(login_body[0], login_body[1]).json()["id"])
 
 @pytest.fixture
 def generate_without_login_data():
@@ -40,13 +40,25 @@ def generate_two_courier_with_same_login_data():
     login_body = LoginMethods.register_new_courier_and_return_login_password(test_body)
     body = login_body[3]
     yield body
-    LoginMethods.delete_login(LoginMethods.login_in_system(login_body[0], login_body[1])[0])
+    LoginMethods.delete_login(LoginMethods.login_in_system(login_body[0], login_body[1]).json()["id"])
 
+#Фикстуры второго задания.
+#Создается логин со случайными параметрами, залогиниваемся получаем id и код ответа, потом удаляем логин из базы.
 @pytest.fixture
 def get_login_id():
     test_body = GenerateBody.generate_body()
     login_body = LoginMethods.register_new_courier_and_return_login_password(test_body)
     body = LoginMethods.login_in_system(login_body[0], login_body[1])
-    print(login_body[0], login_body[1])
     yield body
-    LoginMethods.delete_login(body[0])
+    LoginMethods.delete_login(body.json()["id"])
+
+#Создается логин со случайными параметрами, залогиниваемся получаем id и код ответа, потом удаляем логин из базы.
+@pytest.fixture
+def get_login_withot_password():
+    test_body = GenerateBody.generate_body()
+    login_body = LoginMethods.register_new_courier_and_return_login_password(test_body)
+    body = LoginMethods.login_in_system(login_body[0], login_body[1])
+    print(body.json()["id"])
+    body_answer_withot_pass = LoginMethods.login_in_system(login_body[0], '')
+    yield body_answer_withot_pass
+    LoginMethods.delete_login(body.json()["id"])
--- tests/test_login.py
+++ tests/test_login.py
@@ -5,4 +5,10 @@ class TestLogin:
     @allure.description('Тут создаем логин, входим на сайт, проверяем код ответа')
     def test_courier_authorization(self,get_login_id):
         response = get_login_id
-        assert response[1] == 200
+        assert response.status_code == 200
+
+    @allure.title('Test UnSuccessful courier login without necessary data')
+    @allure.description('Пытаемся войти без пароля и проверяем ответ API')
+    def test_login_courier_without_password(self,get_login_withot_password):
+        response = get_login_withot_password
+        assert response.status_code == 400
