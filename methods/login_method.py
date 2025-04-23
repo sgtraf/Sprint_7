e8000975326fd5e106549551b2ac5401369816b0
G Sh <conan@yandex.ru>
2025-04-23 21:15:07 +0300

    Расставил степ аллюры в методах

--- methods/login_method.py
+++ methods/login_method.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 import urls
 
@@ -13,7 +14,8 @@ class LoginMethods:
         login_pass = []
 
         # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
-        response = requests.post(f'{urls.Url.MAIN_URL}{urls.Url.CREATE_COURIER_URL}', data=body)
+        with allure.step("Создаем запрос создания курьера"):
+            response = requests.post(f'{urls.Url.MAIN_URL}{urls.Url.CREATE_COURIER_URL}', data=body)
 
         #  добавляем в список логин и пароль курьера
         login_pass.append(body['login'])
@@ -27,11 +29,13 @@ class LoginMethods:
     @staticmethod
     def login_in_system(login, password):
         params = {'login': login, 'password': password}
-        response = requests.post(f'{urls.Url.MAIN_URL}{urls.Url.LOGIN_COURIER_URL}/', data = params )
+        with allure.step("Создаем запрос залогинивания курьера"):
+            response = requests.post(f'{urls.Url.MAIN_URL}{urls.Url.LOGIN_COURIER_URL}/', data = params )
         return response
 
     @staticmethod
     def delete_login(login_id):
         params = {'id': login_id}
-        response_delete = requests.delete(f"{urls.Url.MAIN_URL}{urls.Url.DELETE_COURIER_URL}{login_id}", params=params)
+        with allure.step("Создаем запрос удаления курьера"):
+            response_delete = requests.delete(f"{urls.Url.MAIN_URL}{urls.Url.DELETE_COURIER_URL}{login_id}", params=params)
         return response_delete.status_code
--- methods/orders_methods.py
+++ methods/orders_methods.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 import urls
 
@@ -7,11 +8,13 @@ class OrdersMethodes:
     @staticmethod
     # метод создания заказа
     def set_order(order):
-        response = requests.post(f'{urls.Url.MAIN_URL}{urls.Url.CREATE_ORDER_URL}', data=order)
+        with allure.step("Создаем запрос создания заказа"):
+            response = requests.post(f'{urls.Url.MAIN_URL}{urls.Url.CREATE_ORDER_URL}', data=order)
         return response
 
     @staticmethod
     # метод создания заказа
     def cancel_order(track):
-        response = requests.put(f'{urls.Url.MAIN_URL}{urls.Url.CANCEL_ORDER_URL}{track}')
+        with allure.step("Создаем запрос отмены заказа"):
+            response = requests.put(f'{urls.Url.MAIN_URL}{urls.Url.CANCEL_ORDER_URL}{track}')
         return response
